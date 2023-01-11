4e0e75f4456a087de5eccb1c57081b10dae555fb
ProfessorAO <davidodunlade@hotmail.co.uk>
2023-01-11 16:13:28 +0000

    Server side takes in user input from GUI

--- server/index.js
+++ server/index.js
@@ -4,74 +4,113 @@ import { WebSocketServer } from 'ws';
 import http from 'http';
 import { type } from 'os';
 import express from 'express';
+import { Console } from 'console';
 const wrongTypeError = TypeError("Wrong type found, expected ");
+
+async function connection_websockets(){
+    const obj = new Object;
+    const wss = new WebSocketServer({ port:679 });
+    var task_data ;
+    wss.on('connection',async (ws)=>{
+        console.log('New Websocket connection');
+        task_data = await waitForData(ws);
+        openTrapstar(task_data,ws);
+    })
+    wss.on('error',(ws)=>{
+        ws.send('Connection failed');
+    })
+    wss.on('close',(ws)=>{
+        console.log("connection closed");
+    })
+    async function waitForData(ws){
+        return new Promise((resolve, reject) => {
+            ws.on('message', function message(data){
+                console.log('data: %s',data);
+                task_data =  JSON.parse(data);
+                resolve(task_data);
+            });
+        });
+    }
+}
+
+
 function sleep(ms) {
     return new Promise((resolve) => {
       setTimeout(resolve, ms);
     });
 }
 
-function openTrapstar(Product, Size,Keywords,server){
-    Product = "HYPERDRIVE RIVET POCKET HOODIE - BLACK";
-    Size = "M";
+function openTrapstar(task_data,socket){
+   var Product = task_data["product"];
+   var Size = task_data["size"];
 
-    if (typeof Product === 'string' & typeof Size === 'string' & typeof Keywords === 'object') {
+    if (typeof Product === 'string' & typeof Size === 'string') {
         (async () => {
-                    const browser = await puppeteer.launch({headless: true,args: ['--start-maximized', 'disable-gpu', '--disable-infobars', '--disable-extensions', '--ignore-certificate-errors'],});
-                    const page = await browser.newPage();
-                    await page.goto('https://uk.trapstarlondon.com/products.json?limit=1000');
-                    await page.content(); 
-                    var innertxt = await page.evaluate(()=>{
-                        return JSON.parse(document.querySelector("body").textContent);
-                    });
-                    let handle = gethandle(innertxt,Product);
-                    let size_id = getsizeid(innertxt,Product,Size);
-
-                    await page.goto('https://uk.trapstarlondon.com/products/'+handle +'?variant='+size_id );
-                    console.log("Added to cart");
-                    server.on('connection',(socket)=>{
-                        socket.send('Adding to Cart');
-
-                    })
-                    await page.waitForSelector('#AddToCart-product-template');
-                    await page.click('#AddToCart-product-template');
-                    await sleep(1000);
-                    await page.goto('https://uk.trapstarlondon.com/cart');
-                    //await Websocket.send("Checking out");
-                    await page.waitForSelector('input[value="Check out"]');
-                    await page.click('input[value="Check out"]');
-                    await sleep(2000);
-                    await page.waitForSelector('#checkout_shipping_address_first_name');
-                    //region
-                    await page.select("#checkout_shipping_address_country","United Kingdom");
-                    await page.evaluate(() =>{
-                        const first_name = document.querySelector('#checkout_shipping_address_first_name');
-                        const last_name = document.querySelector("#checkout_shipping_address_last_name");
-                        const address = document.querySelector("#checkout_shipping_address_address1");
-                        const city = document.querySelector("#checkout_shipping_address_city");
-                        const postcode = document.querySelector('#checkout_shipping_address_zip');
-                        const p_number = document.querySelector("#checkout_shipping_address_phone");
-                        const email = document.querySelector("#checkout_email");
-
-                        email.value ="davidodunlade@hotmail.co.uk"
-                        first_name.value ="david";
-                        last_name.value = "ade-odunlade";
-                        address.value = "42 Oakwood Avenue";
-                        city.value = "London";
-                        p_number.value = "07984667243";
-                        postcode.value = "BR3 6PJ"
-
-                    });
-                    await sleep(5000);
-                    await page.click("#continue_button");
-                    await page.waitForSelector("#continue_button");
-                    await page.click("#continue_button");
-                    console.log("completed")
-                    await sleep(10000);
-                    await browser.close();
+                    try {
+                        const browser = await puppeteer.launch({headless: true,args: ['--start-maximized', 'disable-gpu', '--disable-infobars', '--disable-extensions', '--ignore-certificate-errors'],});
+                        const page = await browser.newPage();
+                        await page.goto('https://uk.trapstarlondon.com/products.json?limit=1000');
+                        await page.content(); 
+                        var innertxt = await page.evaluate(()=>{
+                            return JSON.parse(document.querySelector("body").textContent);
+                        });
+                        let handle = gethandle(innertxt,Product);
+                        let size_id = getsizeid(innertxt,Product,Size);
+    
+                        await page.goto('https://uk.trapstarlondon.com/products/'+handle +'?variant='+size_id );
+                        await page.waitForSelector('#AddToCart-product-template');
+                        await page.click('#AddToCart-product-template');
+                        console.log("Added to cart");
+                        socket.send('Adding To Cart');
+                        await sleep(1000);
+                        await page.goto('https://uk.trapstarlondon.com/cart');
+                        //await Websocket.send("Checking out");
+                        await page.waitForSelector('input[value="Check out"]');
+                        await page.click('input[value="Check out"]');
+                        await sleep(2000);
+                        await page.waitForSelector('#checkout_shipping_address_first_name');
+                        //region
+                        await page.select("#checkout_shipping_address_country","United Kingdom");
+                        await page.evaluate(() =>{
+                            const first_name = document.querySelector('#checkout_shipping_address_first_name');
+                            const last_name = document.querySelector("#checkout_shipping_address_last_name");
+                            const address = document.querySelector("#checkout_shipping_address_address1");
+                            const city = document.querySelector("#checkout_shipping_address_city");
+                            const postcode = document.querySelector('#checkout_shipping_address_zip');
+                            const p_number = document.querySelector("#checkout_shipping_address_phone");
+                            const email = document.querySelector("#checkout_email");
+    
+                            email.value ="davidodunlade@hotmail.co.uk"
+                            first_name.value ="david";
+                            last_name.value = "ade-odunlade";
+                            address.value = "42 Oakwood Avenue";
+                            city.value = "London";
+                            p_number.value = "07984667243";
+                            postcode.value = "BR3 6PJ";
+    
+                        });
+                        await sleep(5000);
+                        console.log("Checking out");
+                        socket.send('Checking out');
+                        await page.click("#continue_button");
+                        await page.waitForSelector("#continue_button");
+                        await page.click("#continue_button");
+                        console.log("completed");
+                        socket.send('Completed');
+                        await page.screenshot({path: 'C:/Users/david/OneDrive/Documents/GitHub/EpsilonaioGUI/server/success_pics/test.png'});
+                        await sleep(5000);
+                        await browser.close();
+                        
+                    } catch (error) {
+                        console.error("Failed");
+                        socket.send('Failed');
+                        console.log(error.details);
+                        
+                    }
+                   
         })();
     }else{
-        throw wrongTypeError;
+        console.error("Failed");
     }
 };
 
@@ -104,25 +143,7 @@ function getsizeid(jsondata,product,size){
         }
     }
 }
-function connection_websockets(){
-    const obj = new Object;
-    const wss = new WebSocketServer({ port:3000 });
-    console.log("New socket Started....");
-    wss.on('connection',(ws)=>{
-        console.log('New Websocket connection');
-        ws.on('message',function message(data){
-            console.log('data: %s',data);
-        })
-        ws.send('Bot opened');
-        openTrapstar("test","test",obj,wss);
-        
-        
-    })
-    wss.on('error',(ws)=>{
-        ws.send('Connection failed');
-    })
-    
-}
+
 function connection_express(){
     const conn = express();
     const PORT = 3000;
