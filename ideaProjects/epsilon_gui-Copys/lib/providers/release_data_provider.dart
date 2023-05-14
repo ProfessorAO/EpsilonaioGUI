171999381b78124888c4bcbaaa4d201212be7d29
ProfessorAO <davidodunlade@hotmail.co.uk>
2023-05-14 18:19:24 +0100

    minor changes

--- ideaProjects/epsilon_gui-Copys/lib/providers/release_data_provider.dart
+++ ideaProjects/epsilon_gui-Copys/lib/providers/release_data_provider.dart
@@ -48,8 +48,10 @@ class ReleasesData with ChangeNotifier {
 
   List<Map<String, String>> CreateMap(data) {
     for (int i = 0; i <= 5; i++) {
+      String name = data[i]['name'].toString();
+      String cappedName = name.length <= 40 ? name : name.substring(0, 40);
       var map = <String, String>{
-        'name': data[i]['name'].toString(),
+        'name': cappedName,
         'date': data[i]['releaseDate'].toString(),
         'price': data[i]['retailPrice'].toString(),
         'img': data[i]['image']['small'].toString(),
